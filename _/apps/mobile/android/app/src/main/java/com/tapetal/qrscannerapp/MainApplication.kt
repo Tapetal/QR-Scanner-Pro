f16dbb3fb76ead86f6be5351225fb04470cd7b63
Tapetal <aminuashraf55@gmail.com>
2026-03-01 15:34:49 +0100

    fix: init SoLoader before ReleaseLevel to fix libreact_featureflagsjni crash

--- _/apps/mobile/android/app/src/main/java/com/tapetal/qrscannerapp/MainApplication.kt
+++ _/apps/mobile/android/app/src/main/java/com/tapetal/qrscannerapp/MainApplication.kt
@@ -1,8 +1,6 @@
 package com.tapetal.qrscannerapp
-
 import android.app.Application
 import android.content.res.Configuration
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
@@ -12,12 +10,10 @@ import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
-
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
       this,
       object : DefaultReactNativeHost(this) {
@@ -26,26 +22,22 @@ class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
             }
-
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
           override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
       }
   )
-
   override val reactHost: ReactHost
     get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
+    SoLoader.init(this, false)
     DefaultNewArchitectureEntryPoint.releaseLevel = try {
       ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
     } catch (e: IllegalArgumentException) {
       ReleaseLevel.STABLE
     }
-    SoLoader.init(this, false)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
